39026bb5b28588246ff1db344b7ed1dd399e677e
mcdivine2 <146824292+mcdivine2@users.noreply.github.com>
2024-11-16 18:41:22 +0800

    old clearance

--- student/add-request.php
+++ student/add-request.php
@@ -265,17 +265,7 @@
             </div>
         </div>
 
-        <!-- Toast Container -->
-        <div class="toast-container position-fixed bottom-0 end-0 p-3">
-            <div id="successToast" class="toast align-items-center text-bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true">
-                <div class="d-flex">
-                    <div class="toast-body">
-                        Request submitted successfully!
-                    </div>
-                    <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-                </div>
-            </div>
-        </div>
+
 
 
         <!-- ============================================================== -->
