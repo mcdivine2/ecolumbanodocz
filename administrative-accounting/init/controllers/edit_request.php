df62541e727ee7045a93e8ea0ca970ddd262f24c
mcdivine2 <146824292+mcdivine2@users.noreply.github.com>
2024-11-16 22:55:42 +0800

    Update edit_request.php

--- administrative-accounting/init/controllers/edit_request.php
+++ administrative-accounting/init/controllers/edit_request.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../model/class_model.php";
 
-if (isset($_POST)) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$conn = new class_model();
 
 	$control_no = trim($_POST['control_no']);
@@ -13,8 +13,13 @@ if (isset($_POST)) {
 
 	$request = $conn->edit_request($control_no, $student_id, $document_name, $date_request, $accounting_status, $request_id);
 
-	echo '<div class="alert alert-success">Edit Request Successfully!</div>
-	          <script> setTimeout(function() { window.history.go(-1); }, 1000); </script>';
-	echo '<div class="alert alert-danger">Edit Request Failed!</div>
-	          <script> setTimeout(function() { window.history.go(-1); }, 1000); </script>';
+	if ($request) {
+		// Success message
+		echo '<div class="alert alert-success">Edit Request Successfully!</div>
+              <script> setTimeout(function() { window.history.go(-1); }, 1000); </script>';
+	} else {
+		// Failure message
+		echo '<div class="alert alert-danger">Edit Request Failed!</div>
+              <script> setTimeout(function() { window.history.go(-1); }, 1000); </script>';
+	}
 }
