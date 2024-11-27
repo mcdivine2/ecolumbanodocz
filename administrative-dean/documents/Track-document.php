06f6920aefa6621c8abe9f7b9d325b1005099a15
mcdivine2 <146824292+mcdivine2@users.noreply.github.com>
2024-11-27 21:00:32 +0800

    clearance

--- administrative-dean/documents/Track-document.php
+++ administrative-dean/documents/Track-document.php
@@ -92,7 +92,7 @@
                                         'accounting' => 'ACCOUNTING',
                                         'registrar' => 'REGISTRAR'
                                     ];
-                                    if (preg_match("/CBE BOARD EXAM/i", $document['request_type'])) {
+                                    if (preg_match("/Honorable Dismissal w\/ TOR for evaluation/i", $document['document_name'])) {
                                         $departments['dean'] = 'DEAN';
                                     }
 
